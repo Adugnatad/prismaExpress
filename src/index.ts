5c925c59db2d1a7eface1481f41726c66112300a
Adugna Tadesse <adugtad@gmail.com>
2023-09-03 14:21:19 +0300

    latest

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,57 @@ import * as userController from "../controllers/user";
 import * as productController from "../controllers/product";
 import * as orderController from "../controllers/order";
 import { verifyToken } from "../config.ts/jwtToken";
+import { graphqlHTTP } from "express-graphql";
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLNonNull,
+  GraphQLString,
+  GraphQLInt,
+} from "graphql";
+import { Prisma, PrismaClient } from "@prisma/client";
+
 const app = express();
+
+const prisma = new PrismaClient();
+
+const ProductType = new GraphQLObjectType({
+  name: "Product",
+  description: "This is type for Product model",
+  fields: () => ({
+    name: { type: GraphQLString },
+    price: { type: GraphQLInt },
+    available_quantity: { type: GraphQLInt },
+    picture: { type: GraphQLString },
+  }),
+});
+
+const RootQueryType = new GraphQLObjectType({
+  name: "Query",
+  description: "Root Query",
+  fields: () => ({
+    product: {
+      type: ProductType,
+      description: "All Products",
+      resolve: async (parent, args) => {
+        const product = await prisma.product.findMany();
+        return product;
+      },
+    },
+  }),
+});
+
+const schema = new GraphQLSchema({
+  query: RootQueryType,
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
 
 app.use(express.json());
 
@@ -17,6 +67,7 @@ app.get("/test", verifyToken, (req, res) => {
 
 app.post("/product", verifyToken, productController.postProduct);
 app.get("/product", verifyToken, productController.getProducts);
+
 app.get("/product/:id", verifyToken, productController.getProductId);
 
 app.post("/order", verifyToken, orderController.postOrder);
