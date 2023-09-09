580478f631a993e49a998137139eefaea26d7685
Adugna Tadesse <adugtad@gmail.com>
2023-09-09 09:58:32 +0300

    new

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 const app = require("express")();
 const { v4 } = require("uuid");
 
+app.get("/", (req, res) => {
+  res.send("Hey this api is deployed on vercel");
+});
+
 app.get("/test", (req, res) => {
   const path = `/api/item/${v4()}`;
   res.setHeader("Content-Type", "text/html");
@@ -11,6 +15,10 @@ app.get("/test", (req, res) => {
 app.get("/api/item/:slug", (req, res) => {
   const { slug } = req.params;
   res.end(`Item: ${slug}`);
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
 });
 
 module.exports = app;
