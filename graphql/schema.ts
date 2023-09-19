92f2d724e6dc6f057c0fbc3864c71313cf5ca409
Adugna Tadesse <adugtad@gmail.com>
2023-09-19 10:03:24 +0300

    login graphql

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,11 +1,29 @@
 import { PrismaClient } from "@prisma/client";
+import { checkHash } from "../config.ts/hash";
+import jwt from "jsonwebtoken";
+import { secret_key } from "../util/secrets";
 
 const prisma = new PrismaClient();
+
+const generateToken = (username: string) => {
+  const payload = {
+    username: username,
+  };
+  const token = jwt.sign(payload, secret_key, { expiresIn: "1h" });
+  return token;
+};
 
 export const typeDefs = `#graphql
   type Query {
     products: [Product]
     product(id: ID!): Product
+  }
+  type User {
+    id: ID!
+    username: String!
+    password: String!
+    passwordResetToken: String
+    profileId: Int!
   }
   type Product {
     id: ID!
@@ -23,6 +41,7 @@ export const typeDefs = `#graphql
   }
   type Mutation {
     addProduct(product: AddProductInput!): Product
+    login(user: UserInfo!): String
   }
   input AddProductInput {
     name: String!
@@ -30,7 +49,10 @@ export const typeDefs = `#graphql
     available_quantity: Int!
     picture: String
   }
-
+  input UserInfo {
+    username: String!
+    password: String!
+  }
 `;
 
 export const resolvers = {
@@ -55,6 +77,25 @@ export const resolvers = {
         data: product,
       });
       return createdProduct;
+    },
+    async login(_: any, args: any) {
+      console.log(args);
+      const user = await prisma.user.findUnique({
+        where: {
+          username: args.user.username,
+        },
+      });
+      if (user) {
+        const passwordCheck = checkHash(args.user.password, user.password);
+        if (passwordCheck) {
+          const token = generateToken(user.username);
+          return token;
+        } else {
+          return "Invalid credentials";
+        }
+      } else {
+        return "user not found";
+      }
     },
   },
 };
